c4cb0ec85717c3bc43d0547a8ca0efcfab46a42e
Joon Park <jsp220@gmail.com>
2022-10-09 10:04:58 -0700

    Set up server.js with sequelize, express, and handlebars

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: "Temperature Leak Unaware",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`App is listening on Port ${PORT}`);
+    sequelize.sync({ force: false });
+});
