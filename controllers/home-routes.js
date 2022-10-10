7d20e86f44ea1a046f35222791024549754a3ce0
Joon Park <jsp220@gmail.com>
2022-10-10 16:20:20 -0700

    Create handlebars for main page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,16 +4,38 @@ const { Post, Comment, User } = require ('../models/');
 router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll({
-            include: [User, Comment],
+            include: [User]
+        });
+        const plainData = postData.map((post) => post.get({ plain: true }));
+
+        const posts = plainData.map((post) => {
+            delete post.User.password;
+            return post;
         });
 
-        const posts = postData.map((post) => post.get({ plain: true }));
-
-        // res.render('all-posts', { posts });
-        res.json(posts);
+        console.log(posts);
+        res.render('all-posts', { posts });
+        // res.json(posts);
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [User, Comment]
+        });
+
+        const post = postData.get({ plain: true });
+
+        delete post.User.password;
+
+        res.render('single-post', { post });
+        // res.json(post);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
