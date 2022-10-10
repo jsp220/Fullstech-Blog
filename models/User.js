0ebcceb758dafd72ab45776658c1a5d5551a5e27
Joon Park <jsp220@gmail.com>
2022-10-10 01:15:04 -0700

    Create seeds to test back end, test models

--- models/User.js
+++ models/User.js
@@ -30,9 +30,16 @@ User.init(
     },
     {
         hooks: {
+            beforeBulkCreate: (newUsers) => {
+                const saltRounds = 10;
+                newUsers.forEach((user) => {
+                    user.password = bcrypt.hashSync(user.password, saltRounds);
+                })
+            },
             beforeCreate: async (newUser) => {
                 const saltRounds = 10;
                 newUser.password = await bcrypt.hash(newUser.password, saltRounds);
+                console.log(newUser.password);
                 return newUser;
             },
             beforeUpdate: async (updatedUser) => {
