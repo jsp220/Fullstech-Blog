5524b4da208605411f5316f0bbc92619c9134983
Joon Park <jsp220@gmail.com>
2022-10-10 00:28:16 -0700

    Install mysql2, Add more models

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword (pswd) {
+        return bcrypt.compareSync(pswd, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUser) => {
+                const saltRounds = 10;
+                newUser.password = await bcrypt.hash(newUser.password, saltRounds);
+                return newUser;
+            },
+            beforeUpdate: async (updatedUser) => {
+                const saltRounds = 10;
+                updatedUser.password = await bcrypt.hash(updatedUser.password, saltRounds);
+                return updatedUser;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'User'
+    }
+);
+
+module.exports = User;
