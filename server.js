2d357db5572850d8bf7c67cd279379e64b259897
Joon Park <jsp220@gmail.com>
2022-10-09 23:32:03 -0700

    Create controllers/routes structure

--- server.js
+++ server.js
@@ -2,33 +2,46 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const routes = require('./controllers');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-    secret: "Temperature Leak Unaware",
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
+// const sess = {
+//     secret: "Temperature Leak Unaware",
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-app.listen(PORT, () => {
-    console.log(`App is listening on Port ${PORT}`);
-    sequelize.sync({ force: false });
-});
+sequelize.sync({ force: false}).then(() => {
+    app.listen(PORT, () =>
+        console.log(
+            `\nServer running on ${PORT}!`
+            )
+    );
+})
+
+// app.listen(PORT, () => {
+//     console.log(`App is listening on Port ${PORT}`);
+//     sequelize.sync({ force: false });
+// });
